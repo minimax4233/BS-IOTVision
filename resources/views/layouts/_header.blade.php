358071d66821fc1cdac955887334e96d0222b5c3
minimax4233 <minimax4233@gmail.com>
2021-06-24 02:52:44 +0800

    User Login/logout Basic Finish

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,10 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
+    <div class="container">
         <a class="navbar-brand" href="{{ route('home') }}">物联网可视化</a>
         <ul class="navbar-nav justify-content-end">
+            @if (Auth::check())
             <li class="nav-item"><a class="nav-link" href="{{ route('vision') }}">展示</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">用户列表</a></li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{ Auth::user()->name }}
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
+                    <a class="dropdown-item" href="#">编辑资料</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" id="logout" href="#">
+                        <form action="{{ route('logout') }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button class="btn btn-block btn-danger" type="submit" name="button">登出</button>
+                        </form>
+                    </a>
+                </div>
+            </li>
+            @else
+            
+            <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ route('signup') }}">注冊</a></li>
-          <li class="nav-item" ><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+            @endif
         </ul>
-      </div>
-    </nav>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'StaticPagesController@home')->name('home');
 //Route::get('/signup', 'StaticPagesController@signup')->name('signup');
-Route::get('/login', 'StaticPagesController@login')->name('login');
+//Route::get('/login', 'StaticPagesController@login')->name('login');
 Route::get('/vision', 'StaticPagesController@vision')->name('vision');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('signup', 'UsersController@create')->name('signup');
-Route::resource('users', 'UsersController');
-Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// 注冊
+Route::get('signup', 'UsersController@create')->name('signup'); // 创建用户的页面
+//Route::resource('users', 'UsersController');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show'); //显示用户个人信息的页面
+Route::post('/users', 'UsersController@store')->name('users.store'); // 创建用户
+
+// 登录
+Route::get('login', 'SessionsController@create')->name('login'); // 显示登录页面
+Route::post('login', 'SessionsController@store')->name('login'); // 创建新会话（登录）
+Route::delete('logout', 'SessionsController@destroy')->name('logout'); // 销毁会话（退出登录）
