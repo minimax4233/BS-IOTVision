e95250383d6f415d0efcf18867c78a9697429144
minimax4233 <minimax4233@gmail.com>
2021-06-24 19:26:54 +0800

    Add clients, datas table

---
+++ database/migrations/2021_06_24_104050_create_datas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('alert');
+            $table->string('clientID')->unique();;
+            $table->string('info');
+            $table->double('lat');
+            $table->double('lng');
+            $table->bigInteger('timestamp');
+            $table->integer('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('datas');
+    }
+}
